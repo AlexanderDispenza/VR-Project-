f81e7c2e89f245e30e411a3ca321d29330138134
CrescentLua <45102273+CrescentLua@users.noreply.github.com>
2019-10-19 16:38:25 -0400

    Possible Camera fix?

---
+++ VR Group Project/Assets/UIScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class UIScript : MonoBehaviour
+{
+    public void OnStartClick()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+    }
+
+    public void OnQuitGame()
+    {
+        Application.Quit(); 
+    }
+}
