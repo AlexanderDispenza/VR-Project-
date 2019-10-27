fd6a1f2fc961d211f235e52471e2f4d3d8b8f39b
CrescentLua <45102273+CrescentLua@users.noreply.github.com>
2019-10-27 14:17:06 -0400

    Updated Location 1

---
+++ VR Group Project/Assets/Scripts/LocationGazeScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+using Valve.VR;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LocationGazeScript : MonoBehaviour
+{
+    public bool isSelected;
+    public bool timedClick = true;
+    public bool clicker = true;
+    public bool highlight = true;
+
+    void Awake()
+    {
+       
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
